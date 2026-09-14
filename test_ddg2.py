589bd7deffb5bd23a61b26c4231b9ff04ee5573a
artartemev <79992180108@ya.ru>
2026-09-14 19:20:11 +0700

    security: remove internal scrapers and telegram tokens from git

--- test_ddg2.py
+++
@@ -1,8 +0,0 @@
-from duckduckgo_search import DDGS
-print("Starting search...")
-try:
-    with DDGS() as ddgs:
-        res = list(ddgs.images("Waree Onsen Chiang Mai", max_results=1))
-        print("Result:", res)
-except Exception as e:
-    print("Error:", e)
